376358c2cd2f1c2868445c06d4c7e68fdf9d0f93
Braulio SG <soloriobraulio03@gmail.com>
2021-08-31 08:18:03 -0500

    factores

--- factores.cpp
+++ factores.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int main(){
-    int N = 0, f = 0;
+    int numero = 0, numeroFactores = 0;
 
-    cout << "Ingrese N: "; cin >> N;
-    cout << "Ingrese n de factores "; cin >> f;
+    cout << "Ingrese N: "; cin >> numero;
+    cout << "Ingrese n de factores "; cin >> numeroFactores;
 
-    for (int i = 1; i <= f; i ++ ){
-        int r = N * i; 
-        cout << N << "x" << i << "= " << r << endl;
+    for (int i = 1; i <= numeroFactores; i ++ ){
+        int resultado = numero * i; 
+        cout << numero << " x " << i << " = " << resultado << endl;
     }
 }
